3b90aeb0c589f267b325b647687d16c917c38f17
Quetzotl <91239910+BigZnakie@users.noreply.github.com>
2026-03-12 13:18:34 -0700

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <SDL3/SDL.h>
 #define SDL_MAIN_USE_CALLBACKS
 #include <SDL3/SDL_main.h>
-#include <SDL3/SDL_image.h>
+#include <SDL3_image/SDL_image.h>
 
 SDL_Window* window;
 SDL_Renderer* renderer;
